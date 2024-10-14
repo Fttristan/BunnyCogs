516491061176817de8a560b0672fd5770859ef80
FTTristan <135572396+Fttristan@users.noreply.github.com>
2024-10-13 20:51:31 -0400

    Update antiraid.py
    Fix accidental bug when i updated the url for the service i appended /v1 for the api, which caused all banchecks to say no ban found

--- bancheck/services/antiraid.py
+++ bancheck/services/antiraid.py
@@ -15,9 +15,9 @@ class Antiraid:
 
     SERVICE_NAME = "Bunnybot Gban"
     SERVICE_API_KEY_REQUIRED = False
-    SERVICE_URL = "http://api.bunnybot.org/v1/_/"
+    SERVICE_URL = "http://api.bunnybot.org/_/"
     SERVICE_HINT = None
-    BASE_URL = "http://api.bunnybot.org/v1/api/collections"
+    BASE_URL = "http://api.bunnybot.org/api/collections"
 
     @staticmethod
     async def lookup(user_id: int, _api_key: str) -> LookupResult:
