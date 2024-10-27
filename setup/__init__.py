3a6d9cd9ff97a968688577036401c782c2475141
FTTristan <135572396+Fttristan@users.noreply.github.com>
2024-10-26 21:50:32 -0400

    Update __init__.py

--- setup/__init__.py
+++ setup/__init__.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 from redbot.core.bot import Red
 
-from .setup import Setup
+from .setup import Setup2
 
 with Path(__file__).parent.joinpath("info.json").open() as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
@@ -13,6 +13,6 @@ with Path(__file__).parent.joinpath("info.json").open() as fp:
 
 async def setup(bot: Red) -> None:
     """Load Setup cog."""
-    cog = Setup(bot)
+    cog = Setup2(bot)
     await cog.initialize()
     await bot.add_cog(cog)
