e5763de1810fb2b757a250199827221cd6fa2a9b
FTTristan <135572396+Fttristan@users.noreply.github.com>
2024-10-26 22:01:00 -0400

    Update __init__.py

--- setup/__init__.py
+++ setup/__init__.py
@@ -11,8 +11,5 @@ with Path(__file__).parent.joinpath("info.json").open() as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
-async def setup(bot: Red) -> None:
-    """Load Setup cog."""
-    cog = Setup2(bot)
-    await cog.initialize()
-    await bot.add_cog(cog)
+async def setup(bot):
+    await bot.add_cog(Setup2(bot))
