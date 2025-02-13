a5c5854eb91351a3460249a7b24c3dc6ed7f6c2a
FTTristan <135572396+Fttristan@users.noreply.github.com>
2025-02-13 06:29:27 +0000

    update errors

--- verify/verify.py
+++ verify/verify.py
@@ -95,13 +95,17 @@ class VerificationCog(commands.Cog):
                                         embed.add_field(name="VRChat ID", value=data['items'][0]['vrchat_id'])
                                         await data_channel.send(embed=embed)
                                 else:
-                                    await ctx.send("Verification role not found.")
+                                    if verification_channel:
+                                        await verification_channel.send(f"{member.mention}, please verify yourself at https://verify.vrcband.com/api/v1/lantern/consent/{api_key}")
                             else:
-                                await ctx.send("Verification failed. User ID does not match.")
+                                if verification_channel:
+                                    await verification_channel.send(f"{member.mention}, please verify yourself at https://verify.vrcband.com/api/v1/lantern/consent/{api_key}")
                         else:
-                            await ctx.send("Verification failed. API Key does not match.")
+                            if verification_channel:
+                                await verification_channel.send(f"{member.mention}, please verify yourself at https://verify.vrcband.com/api/v1/lantern/consent/{api_key}")
                     else:
-                        await ctx.send("Verification failed. No items found.")
+                        if verification_channel:
+                            await verification_channel.send(f"{member.mention}, please verify yourself at https://verify.vrcband.com/api/v1/lantern/consent/{api_key}")
             except aiohttp.ClientConnectionError:
                 await ctx.send("Verification failed. Could not connect to host.")
             except aiohttp.ClientError as e:
